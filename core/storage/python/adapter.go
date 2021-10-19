ce09db178c5c06ff55f224d91b9c9103f499264c
Dylan Deng <dxx1104@gmail.com>
2021-10-20 00:48:47 +0800

    support invoke storage by python

---
+++ core/storage/python/adapter.go
@@ -0,0 +1,242 @@
+package main
+
+import "C"
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"strings"
+	"sync"
+	"unsafe"
+
+	"github.com/dxxzx/magnifier/core/storage"
+	"github.com/dxxzx/magnifier/core/storage/factory"
+	_ "github.com/dxxzx/magnifier/core/storage/filesystem"
+	"gopkg.in/yaml.v2"
+)
+
+var driverHolder struct {
+	lock    sync.Mutex
+	drivers map[C.int]storage.Driver
+	idx     C.int
+}
+var readerHolder struct {
+	lock    sync.Mutex
+	readers map[C.int]io.ReadCloser
+	idx     C.int
+}
+var writerHolder struct {
+	lock    sync.Mutex
+	writers map[C.int]io.WriteCloser
+	idx     C.int
+}
+
+func init() {
+	driverHolder.lock.Lock()
+	defer driverHolder.lock.Unlock()
+	driverHolder.drivers = make(map[C.int]storage.Driver)
+	driverHolder.idx = 0
+
+	readerHolder.lock.Lock()
+	defer readerHolder.lock.Unlock()
+	readerHolder.readers = make(map[C.int]io.ReadCloser)
+	readerHolder.idx = 0
+
+	writerHolder.lock.Lock()
+	defer writerHolder.lock.Unlock()
+	writerHolder.writers = make(map[C.int]io.WriteCloser)
+	writerHolder.idx = 0
+}
+
+//export CreateStorageDriver
+func CreateStorageDriver(name *C.char, parameters *C.char) C.int {
+	raw_params := C.GoString(parameters)
+	params := make(map[string]interface{})
+	err := yaml.Unmarshal([]byte(raw_params), params)
+	if err != nil {
+		panic(err.Error())
+	}
+	driver, err := factory.Create(C.GoString(name), params)
+	if err != nil {
+		panic(err.Error())
+	}
+	driverHolder.lock.Lock()
+	defer driverHolder.lock.Unlock()
+	idx := driverHolder.idx
+	driverHolder.idx++
+	driverHolder.drivers[idx] = driver
+
+	return idx
+}
+
+func getDriver(idx C.int) storage.Driver {
+	driverHolder.lock.Lock()
+	defer driverHolder.lock.Unlock()
+	return driverHolder.drivers[idx]
+}
+
+//export ReleaseDriver
+func ReleaseDriver(idx C.int) {
+	driverHolder.lock.Lock()
+	defer driverHolder.lock.Unlock()
+	delete(driverHolder.drivers, idx)
+}
+
+//export GetContent
+func GetContent(idx C.int, path *C.char) unsafe.Pointer {
+	driver := getDriver(idx)
+	data, err := driver.GetContent(C.GoString(path))
+	if err != nil {
+		panic(err.Error())
+	}
+	return C.CBytes(data)
+}
+
+//export PutContent
+func PutContent(idx C.int, path *C.char, content unsafe.Pointer, len C.int) {
+	driver := getDriver(idx)
+	err := driver.PutContent(C.GoString(path), C.GoBytes(content, len))
+	if err != nil {
+		panic(err)
+	}
+}
+
+//export Reader
+func Reader(idx C.int, path *C.char, offset C.longlong) C.int {
+	driver := getDriver(idx)
+	reader, err := driver.Reader(C.GoString(path), int64(offset))
+	if err != nil {
+		panic(err)
+	}
+	readerHolder.lock.Lock()
+	defer readerHolder.lock.Unlock()
+	ridx := readerHolder.idx
+	readerHolder.idx++
+	readerHolder.readers[ridx] = reader
+	return ridx
+}
+
+//export Writer
+func Writer(idx C.int, path *C.char, append C._Bool) C.int {
+	driver := getDriver(idx)
+	writer, err := driver.Writer(C.GoString(path), bool(append))
+	if err != nil {
+		panic(err)
+	}
+	writerHolder.lock.Lock()
+	defer writerHolder.lock.Unlock()
+	widx := writerHolder.idx
+	writerHolder.idx++
+	writerHolder.writers[widx] = writer
+	return widx
+}
+
+//export Stat
+func Stat(idx C.int, path *C.char) *C.char {
+	driver := getDriver(idx)
+	stat, err := driver.Stat(C.GoString(path))
+	if err != nil {
+		panic(err)
+	}
+	resultStr := fmt.Sprintf(
+		"%s:%d:%d:%t",
+		stat.Name(),
+		stat.ModTime().Unix(),
+		stat.Size(),
+		stat.IsDir(),
+	)
+	return C.CString(resultStr)
+}
+
+//export List
+func List(idx C.int, path *C.char) *C.char {
+	driver := getDriver(idx)
+	result, err := driver.List(C.GoString(path))
+	if err != nil {
+		panic(err)
+	}
+	resultStr := strings.Join(result, "\n")
+	return C.CString(resultStr)
+}
+
+//export Move
+func Move(idx C.int, sourcePath *C.char, destPath *C.char) {
+	driver := getDriver(idx)
+	err := driver.Move(C.GoString(sourcePath), C.GoString(destPath))
+	if err != nil {
+		panic(err)
+	}
+}
+
+//export Delete
+func Delete(idx C.int, path *C.char) {
+	driver := getDriver(idx)
+	err := driver.Delete(C.GoString(path))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getReader(idx C.int) io.ReadCloser {
+	readerHolder.lock.Lock()
+	defer readerHolder.lock.Unlock()
+	return readerHolder.readers[idx]
+}
+
+//export Read
+func Read(idx C.int, buffer unsafe.Pointer, size C.int) C.int {
+	reader := getReader(idx)
+	var bufHolder []byte
+	bufPtr := (*reflect.SliceHeader)(unsafe.Pointer(&bufHolder))
+	bufPtr.Data = uintptr(buffer)
+	bufPtr.Len = int(size)
+	bufPtr.Cap = int(size)
+	n, err := reader.Read(bufHolder)
+	if err != nil {
+		if n == 0 && err != io.EOF {
+			panic(err)
+		}
+	}
+	return C.int(n)
+}
+
+//export CloseReader
+func CloseReader(idx C.int) {
+	reader := getReader(idx)
+	if reader != nil {
+		defer reader.Close()
+	}
+	readerHolder.lock.Lock()
+	defer readerHolder.lock.Unlock()
+	delete(readerHolder.readers, idx)
+}
+
+func getWriter(idx C.int) io.WriteCloser {
+	writerHolder.lock.Lock()
+	defer writerHolder.lock.Unlock()
+	return writerHolder.writers[idx]
+}
+
+//export Write
+func Write(idx C.int, buf unsafe.Pointer, size C.int) C.int {
+	writer := getWriter(idx)
+	n, err := writer.Write(C.GoBytes(buf, size))
+	if err != nil {
+		panic(err)
+	}
+	return C.int(n)
+}
+
+//export CloseWriter
+func CloseWriter(idx C.int) {
+	writer := getWriter(idx)
+	if writer != nil {
+		defer writer.Close()
+	}
+	writerHolder.lock.Lock()
+	defer writerHolder.lock.Unlock()
+	delete(writerHolder.writers, idx)
+}
+
+func main() {
+}
---
+++ core/storage/python/client.py
@@ -0,0 +1,141 @@
+import ctypes
+import io
+import time
+
+IMPL_LIB = 'libstorage.so'
+_impl = ctypes.CDLL(IMPL_LIB)
+_libc = ctypes.CDLL('libc.so.6')
+
+
+def _str2p(s):
+    return ctypes.c_char_p(bytes(s, encoding='utf-8'))
+
+
+class Reader(io.IOBase):
+    def __init__(self, impl) -> None:
+        self.impl = impl
+
+    def seekable(self) -> bool:
+        return False
+
+    def readable(self) -> bool:
+        return True
+
+    def writable(self) -> bool:
+        return False
+
+    def close(self) -> None:
+        _impl.CloseReader(self.impl)
+
+    def read(self, size=-1):
+        if size is None:
+            size = -1
+        buf = ctypes.create_string_buffer(size)
+        len = _impl.Read(self.impl, buf, ctypes.c_int(size))
+        return buf[0:len]
+
+
+class Writer(io.IOBase):
+    def __init__(self, impl) -> None:
+        self.impl = impl
+
+    def seekable(self) -> bool:
+        return False
+
+    def readable(self) -> bool:
+        return False
+
+    def writable(self) -> bool:
+        return True
+
+    def close(self) -> None:
+        _impl.CloseWriter(self.impl)
+
+    def write(self, buffer):
+        length = _impl.Write(
+            self.impl,
+            ctypes.c_char_p(buffer),
+            ctypes.c_int(len(buffer)),
+        )
+        return length
+
+
+class Stat(object):
+    name: str
+    mod_time: time.struct_time
+    size: int
+    is_dir: bool
+
+    def __init__(self, name, mod_time, size, is_dir) -> None:
+        self.name = name
+        self.mod_time = mod_time
+        self.size = size
+        self.is_dir = is_dir
+
+    def __str__(self):
+        return f'name:{self.name}\nmod_time:{self.mod_time}\nsize:{self.size}\nis_dir:{self.is_dir}'
+
+
+class Storage(object):
+    def __init__(self, configs):
+        self.driver = _impl.CreateStorageDriver(
+            _str2p('filesystem'),
+            _str2p(configs),
+        )
+
+    def get_content(self, path):
+        p = _impl.GetContent(self.driver, _str2p(path))
+        result = ctypes.string_at(p)
+        _libc.free(p)
+        return result
+
+    def put_content(self, path, data):
+        _impl.PutContent(
+            self.driver,
+            _str2p(path),
+            ctypes.c_char_p(data),
+            ctypes.c_int(len(data)),
+        )
+
+    def reader(self, path, offset):
+        impl = _impl.Reader(
+            self.driver,
+            _str2p(path),
+            ctypes.c_int(offset),
+        )
+        return Reader(impl)
+
+    def writer(self, path, append):
+        impl = _impl.Writer(
+            self.driver,
+            _str2p(path),
+            ctypes.c_bool(append)
+        )
+        return Writer(impl)
+
+    def stat(self, path):
+        p = _impl.Stat(self.driver, _str2p(path))
+        s = ctypes.string_at(p)
+        _libc.free(p)
+        name, mod, size, is_dir = str(s, encoding='utf-8').split(':')
+        return Stat(
+            name,
+            time.localtime(int(mod, 10)),
+            int(size, 10),
+            bool(is_dir),
+        )
+
+    def list(self, path):
+        p = _impl.List(self.driver, _str2p(path))
+        result = ctypes.string_at(p)
+        _libc.free(p)
+        return str(result, encoding='utf-8').split('\n')
+
+    def move(self, src, dst):
+        _impl.Move(self.driver, _str2p(src), _str2p(dst))
+
+    def delete(self, path):
+        _impl.Delete(self.driver, _str2p(path))
+
+    def close(self):
+        _impl.ReleaseDriver(self.driver)
